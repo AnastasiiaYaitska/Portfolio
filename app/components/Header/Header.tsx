63ad073d626271e541121c5bf08c43c915380755
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-07-21 15:13:01 +0200

    styled header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -6,30 +6,34 @@ import Link from "next/link";
 const Header = ({ data }) => {
   const { name, pages } = data;
   return (
-    <header>
-      <div>
-        <nav>
-          <ul>
+    <header className="flex flex-col relative bg-linear-fc5c7d w-full h-32 py-6  bg-header">
+      <div className="flex flex-col justify-around h-full px-6  z-10">
+        <nav className="h-full">
+          <ul className="text-right flex flex-row gap-6 justify-end h-full">
             {pages.map(({ id, label, link }) => (
-              <li key={id}>
-                <Link aria-label={label} href={link} className="">
+              <li key={id} className=" ">
+                <Link
+                  aria-label={label}
+                  href={link}
+                  className="px-6 py-2 glow-on-hover"
+                >
                   {label}
                 </Link>
               </li>
             ))}
           </ul>
         </nav>
-        <h1>{name}</h1>
+        <h1 className="text-white uppercase  text-4xl">{name}</h1>
       </div>
-      <Image
+      {/* <Image
         priority
-        height={200}
-        width={2000}
-        className=""
-        src="/images/Computer.jpeg"
+        height={80}
+        width={350}
+        className="w-full h-48 object-contain object-right-bottom absolute mb-6"
+        src="/images/PackagingDesignShutterstock.jpeg"
         alt="drawing laptop"
         aria-label="picture"
-      />
+      /> */}
     </header>
   );
 };
