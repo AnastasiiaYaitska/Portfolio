eb8dd71900947ea2b88596aea26a7bb6647f4dea
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-07-24 16:30:00 +0200

    created footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,8 +1,35 @@
 "use client";
 import React from "react";
+import { HiOutlineMail } from "react-icons/hi";
+import { TfiGithub } from "react-icons/tfi";
+import { FiLinkedin } from "react-icons/fi";
 
-const Footer = () => {
-  return <div>footer</div>;
+type FooterProps = {
+  contactMeLinks: string[];
+};
+
+const Footer = ({ contactMeLinks }: FooterProps) => {
+  return (
+    <footer className="w-full bg-wight shadow-footer mt-12 py-4 fixed bottom-0">
+      <ul className="flex item-center justify-center gap-3">
+        <li>
+          <a href={contactMeLinks[0]} aria-label="Email">
+            <HiOutlineMail size="2.2rem" />
+          </a>
+        </li>
+        <li>
+          <a href={contactMeLinks[1]} aria-label="GitHub">
+            <TfiGithub size="2rem" />
+          </a>
+        </li>
+        <li>
+          <a href={contactMeLinks[2]} aria-label="Linkedin">
+            <FiLinkedin size="2rem" />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
