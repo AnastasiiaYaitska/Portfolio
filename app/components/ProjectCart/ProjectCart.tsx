d32750d65de21a036f3263e7cc0bacd2995cfd0a
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-07-27 14:51:31 +0200

    created my projects page,myprojectcard component

---
+++ app/components/ProjectCart/ProjectCart.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { BsWindow } from "react-icons/bs";
+import { TfiGithub } from "react-icons/tfi";
+
+type ProjectCartProps = {
+  project: {
+    title: string;
+    body: string;
+    tools: string;
+    image: string;
+    links: { title: string; url: string }[];
+  };
+};
+
+const ProjectCart = ({ project }: ProjectCartProps) => {
+  const { title, body, tools, image, links } = project;
+  return (
+    <li className="rounded-lg shadow-footer w-full md:w-2/3-g sm:w-1/3-g card-on-hover">
+      <Image
+        alt={`Image of ${title}`}
+        aria-label={`Image of ${title}`}
+        src={`/images/${image}`}
+        width={500}
+        height={250}
+        className="object-fill w-full h-48"
+      />
+      <div className="p-4">
+        <h3 className="text-lg font-medium">{title}</h3>
+        <p className="text-sm mt-2 min-h-50">{body} </p>
+        <p className="text-sm mt-2 min-h-50">Technologies used: {tools}</p>
+        <ul className="flex flex-row mt-4 justify-between">
+          {links.map(({ title, url }) => (
+            <li key={url} className="flex flex-row">
+              <Link
+                href={url}
+                className="flex flex-row justify-center items-center gap-3"
+              >
+                {title.includes("code") ? <TfiGithub /> : <BsWindow />}
+                <span>{title}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </li>
+  );
+};
+
+export default ProjectCart;
--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,7 +1,30 @@
+"use client";
 import React from "react";
+import projects from "../data/projects.json";
+import ProjectCart from "../components/ProjectCart/ProjectCart";
+
+type ProjectProp = {
+  title: string;
+  body: string;
+  tools: string;
+  image: string;
+  links: {
+    title: string;
+    url: string;
+  }[];
+};
 
 const MyProjects = () => {
-  return <div>my projects</div>;
+  return (
+    <section className="flex flex-col w-11/12 content-center m-auto mt-10">
+      <h2 className="">My Projects</h2>
+      <ul className="flex flex-row justify-center flex-wrap gap-10 mt-6">
+        {projects.map((project: ProjectProp) => {
+          return <ProjectCart project={project} key={project.title} />;
+        })}
+      </ul>
+    </section>
+  );
 };
 
 export default MyProjects;
