f85459de71413f690f178859c3ed123d4e162128
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-07-31 22:21:30 +0200

    add responsive styles

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -7,12 +7,12 @@ const ContactMe = () => {
   return (
     <div className="flex flex-col w-11/12 content-center m-auto mt-10">
       <h2>Contact me</h2>
-      <div className="flex flex-row w-9/12 content-center m-auto px-20 py-10 shadow-footer gap-20">
-        <div className="flex flex-col w-2/4">
+      <div className="flex sm:flex-row flex-col w-9/12 content-center m-auto shadow-footer gap-20 px-10 py-5 sm:px-20 sm:py-10 ">
+        <div className="flex flex-col sm:w-2/4 w-full">
           <h3 className="text-2xl mb-10">{ContactMe.title}</h3>
           <p>{ContactMe.body}</p>
         </div>
-        <div className="w-2/4">
+        <div className="sm:w-2/4 w-full">
           <Form />
         </div>
       </div>
