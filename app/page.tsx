36345e8aa16882394e3e430a561785cda50bfae5
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-08-03 16:01:48 +0200

    update component path

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import data from "./data/data.json";
-import DownloadBtn from "./components/DownloadBtn/downloadBtn";
+import DownloadBtn from "./components/DownloadBtn/DownloadBtn";
 
 export default function Home() {
   return (
