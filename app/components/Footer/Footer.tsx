8e75d7894a75cfafe6b098743e69c7dc032d0179
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-08-24 13:40:38 +0200

    add target blank in footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 import { HiOutlineMail } from "react-icons/hi";
 import { TfiGithub } from "react-icons/tfi";
 import { FiLinkedin } from "react-icons/fi";
@@ -13,19 +14,19 @@ const Footer = ({ contactMeLinks }: FooterProps) => {
     <footer className="w-full bg-wight shadow-footer mt-12 py-4 ">
       <ul className="flex item-center justify-center gap-3">
         <li>
-          <a href={contactMeLinks[0]} aria-label="Email">
+          <Link href={contactMeLinks[0]} aria-label="Email" target="_blank">
             <HiOutlineMail size="2.2rem" />
-          </a>
+          </Link>
         </li>
         <li>
-          <a href={contactMeLinks[1]} aria-label="GitHub">
+          <Link href={contactMeLinks[1]} aria-label="GitHub" target="_blank">
             <TfiGithub size="2rem" />
-          </a>
+          </Link>
         </li>
         <li>
-          <a href={contactMeLinks[2]} aria-label="Linkedin">
+          <Link href={contactMeLinks[2]} aria-label="Linkedin" target="_blank">
             <FiLinkedin size="2rem" />
-          </a>
+          </Link>
         </li>
       </ul>
     </footer>
