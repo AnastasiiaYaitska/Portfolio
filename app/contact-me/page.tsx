25610b629a2556e03e6974ba2b547e9d5d8f4610
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-08-24 13:41:19 +0200

    changed form padding

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -7,7 +7,7 @@ const ContactMe = () => {
   return (
     <div className="flex flex-col w-11/12 content-center m-auto mt-10">
       <h2>Contact me</h2>
-      <div className="flex sm:flex-row flex-col w-9/12 content-center m-auto shadow-footer gap-20 px-10 py-5 sm:px-20 sm:py-10 ">
+      <div className="flex sm:flex-row flex-col w-9/12 content-center m-auto shadow-footer gap-20 px-5 py-5 sm:px-10 sm:py-10 ">
         <div className="flex flex-col sm:w-2/4 w-full">
           <h3 className="text-2xl mb-10">{ContactMe.title}</h3>
           <p>{ContactMe.body}</p>
