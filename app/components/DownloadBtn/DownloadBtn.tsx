ca963774b01d9c9fbeffed34d82fa6cbcc19dfc3
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-08-24 13:22:18 +0200

    update cv

--- app/components/DownloadBtn/DownloadBtn.tsx
+++ app/components/DownloadBtn/DownloadBtn.tsx
@@ -11,7 +11,7 @@ const DownloadBtn = () => {
   return (
     <div className="flex aline-center justify-center sm:justify-start">
       <Link
-        href="./cv/Anastasia.CV.pdf"
+        href="./cv/AnastasiaCV.2.pdf"
         download
         onClick={handleClick}
         className={isClicked ? " clickButton clicked" : "clickButton"}
